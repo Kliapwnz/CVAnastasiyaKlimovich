5844e1b76edd9b5638d437ff03f63b14f10fb24d
Evgeniy Klimovch <kliasanik@gmail.com>
2024-01-09 13:41:04 +0300

    first commit

---
+++ src/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+            <div>
+                <h2>Anastasiya Klimovich</h2>
+                <h3>Web, UX/UI designer</h3>
+                <p><b>Minsk, Belarus</b></p>
+                <p>Hello, I am a Web, UX/UI designer with 1,5 years experience on commercial projects. I also have 1.5
+                    years of experience as SEO-assistant.</p>
+            </div>
+            <div>
+                <img src="../pictures/nastya.jpg"/>
+            </div>
+        </div>
+    );
+};
+
